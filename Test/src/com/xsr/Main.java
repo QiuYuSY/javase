0fff07103366d3117badaf57b9000dc2ddad54d9
TR <2448567284@qq.com>
2020-09-17 12:03:05 +0800

    Commit Message

---
+++ Test/src/com/xsr/Main.java
@@ -0,0 +1,21 @@
+package com.xsr;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int time = 1;
+        String input = s.nextLine();
+
+        while(s.nextLine().equals(input)){
+            time++;
+        }
+        if(time > 2){
+            System.out.println(time-2);
+            return;
+        }
+        System.out.println("oJ Group Great Again!");
+        System.out.println("github test again");
+    }
+}
